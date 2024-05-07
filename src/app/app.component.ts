7bdab2571514197a1fb25a12b8d7bc9b097e1709
Navin <navintechwork@gmail.com>
2024-05-07 13:49:25 +0530

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { CalendarOptions } from '@fullcalendar/core';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import interactionPlugin from '@fullcalendar/daygrid'
+// import interactionPlugin from '@fullcalendar/interaction';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +11,46 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-fullcalendar';
+  Events: any[] = [];
+  calendarOptions: CalendarOptions = {
+    plugins: [dayGridPlugin, interactionPlugin],
+    initialView: 'dayGridMonth',
+    headerToolbar: {
+      left: 'prev,next today',
+      center: 'title',
+      right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek',
+    },
+    weekends: true,
+    editable: true,
+    selectable: true,
+    selectMirror: true,
+    dayMaxEvents: true,
+  };
+  constructor(private httpClient: HttpClient) {}
+  onDateClick(res: any) {
+    alert('Clicked on date : ' + res.dateStr);
+  }
+  ngOnInit() {
+    setTimeout(() => {
+      return this.httpClient
+        .get('http://localhost:8888/event.php')
+        .subscribe((res: any) => {
+          this.Events.push(res);
+          console.log(this.Events);
+        });
+    }, 2200);
+    // setTimeout(() => {
+    //   this.calendarOptions = {
+    //     initialView: 'dayGridMonth',
+    //     events: this.Events,
+    //     dateClick: this.onDateClick.bind(this),
+    //   };
+    // }, 2500);
+    setTimeout(() => {
+      this.calendarOptions = {
+        initialView: 'dayGridMonth',
+        events: this.Events,
+      };
+    }, 2500);
+  }
 }
